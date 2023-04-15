150c2d24046d28e4b202295c657e9a3781bbbc4f
Emma <cemma313@gmail.com>
2023-04-15 10:21:11 -0600

    added schemas

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+
+
 
 function App() {
   return (
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,3 @@
+const { User } = require("../models");
+const { getToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
